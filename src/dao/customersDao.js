7cf63827cb941eabb953f72f3785181573778202
KerrieMeister96 <koensmit13@gmail.com>
2025-09-07 23:01:19 +0200

    moved query's, added whole crud

--- src/dao/customersDao.js
+++ src/dao/customersDao.js
@@ -1,22 +1,49 @@
-const data = require('../db/sql/exampleData');
 const connection = require('../db/sql/connection');
+const queries = require('../db/query');
 
 const customersDao = {
-    get: (customerId, callback) => {
-        connection.query(
-            customerId == undefined
-                ? (`SELECT * FROM ??`)
-                : (`SELECT * FROM ?? WHERE ?? = ?`),
-            ['customer', 'customer_id', customerId],
-            (err, results) => {
-                if (err) return callback(err, undefined);
-                if (results) return callback(undefined, results);
-            }
-        );
-    },
-    delete: () => {
-        // connection.query('DELETE FROM customer WHERE customer_id = ?', [id], (err, result) => {
+  get: (customerId, callback) => {
+    if (customerId === undefined) {
+      connection.query(queries.customer.getAll, (err, results) => {
+        if (err) return callback(err, undefined);
+        return callback(undefined, results);
+      });
+    } else {
+      connection.query(queries.customer.getById, [customerId], (err, results) => {
+        if (err) return callback(err, undefined);
+        return callback(undefined, results);
+      });
     }
+  },
+
+  create: (firstName, lastName, callback) => {
+    connection.query(
+      queries.customer.create,
+      [firstName, lastName],
+      (err, result) => {
+        if (err) return callback(err, undefined);
+        return callback(undefined, result);
+      }
+    );
+  },
+
+  update: (customerId, firstName, lastName, callback) => {
+    connection.query(
+      queries.customer.update,
+      [firstName, lastName, customerId],
+      (err, result) => {
+        if (err) return callback(err, undefined);
+        return callback(undefined, result);
+      }
+    );
+  },
+
+  delete: (customerId, callback) => {
+    connection.query(queries.customer.delete, [customerId], (err, result) => {
+      if (err) return callback(err, undefined);
+      return callback(undefined, result);
+    });
+  },
 };
 
 module.exports = customersDao;
